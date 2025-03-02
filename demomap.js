1af1b0c8911427701fc33b41a6d85f30f00cb3e6
Pauline Thiele <Fenstersteuerungber SupportPauline.Thiele@Student.HTW-Berlin.de>
2025-03-02 12:07:52 +0100

    cleaned demomao.js file and added comments

--- demomap.js
+++ demomap.js
@@ -1,5 +1,14 @@
+/**
+ * ------ Air Quality Website ------
+ * 
+ * Shows the map as a background and includes layers. 
+ * 
+ * Author: Alberding GmbH, Pauline Thiele 
+ */
+
+
 ///////////////////////////////////////////
-// init map
+// init map    -- from Alberding GmbH
 
 var layers = [];
 var overlays = [];
@@ -34,28 +43,27 @@ var layerSwitcher = new ol.control.LayerSwitcher({tipLabel: 'Légende'});
 map.addControl(layerSwitcher);
 
 ///////////////////////////////////////////
-// include WFS
+// include WFS    -- from Alberding GmbH
 
 const vectorSource = new ol.source.Vector({
   format: new ol.format.GeoJSON(),
   //url: 'https://pauline:bachelor2025@smarttrack.gnssonline.eu/cgi-bin/smartTrack.cgi?mod=Status&geojson=1',
-  url: './points.geojson',
+  //url: './points.geojson',
 });
 
 const smarttrack = new ol.layer.Vector({
   source: vectorSource,
   title: "SmartTrack WFS"
 });
-//console.log(vectorSource.getFeatures());
 
 overlayGroup.getLayers().push(smarttrack);
 
 
-///////////////////// by Pauline Thiele begin //////////////////////
-// include air quality 
+///////////////////////////////////////////
+// include air quality    -- from Pauline Thiele
 const dataFile = './hour_created_geodata copy.geojson';
 
-const vectorSource_air = new ol.source.Vector({
+const vectorSourceAir = new ol.source.Vector({
   format: new ol.format.GeoJSON(),
   //url: './air.geojson',
   url: dataFile,
@@ -65,6 +73,12 @@ const vectorSource_air = new ol.source.Vector({
 var colorList = {excellent:'#4d9221', good: '#a1d76a', moderate: '#f7f7f7', weak: '#e9a3c9', poor: '#c51b7d', unknown:'grey',};
 create_AQ_Legend(colorList); // in aq_legend.js
 
+
+/**
+ * Gets color string by air quality level. 
+ * @param {number} airQuality 
+ * @returns {string}
+ */
 function getColorByAirQuality(airQuality) {
   if (airQuality == 1) {
     return colorList.excellent;
@@ -81,6 +95,12 @@ function getColorByAirQuality(airQuality) {
   }
 }
 
+
+/**
+ * Returns the OpenLayers default style for a point based on its air quality index.
+ * @param {object} feature 
+ * @returns {ol.style.Style}
+ */
 const pointStyle = function(feature) { 
   return new ol.style.Style({
     image: new ol.style.Circle({
@@ -92,11 +112,51 @@ const pointStyle = function(feature) {
 };
 
 
+/**
+ * Returns the OpenLayers selected style for a point based on its air quality index.
+ * @param {object} feature 
+ * @returns {ol.style.Style}
+ */
+const pointStyleLarge = function(feature) { 
+  const airQuality = feature.get('Luftqualitätsindex');
+  const color = getColorByAirQuality(airQuality);
+
+  return [
+    // half transparent bigger circle
+    new ol.style.Style({
+      geometry: function (feature) {
+        return feature.getGeometry();
+      },
+      image: new ol.style.Circle({
+        radius: 30,
+        fill: new ol.style.Fill({ color: color + '80' }), 
+        stroke: new ol.style.Stroke({ color: color, width: 1 }),
+      }),
+    }),
+
+    // not transparent smaller circle
+    new ol.style.Style({
+      geometry: function (feature) {
+        return feature.getGeometry();
+      },
+      image: new ol.style.Circle({
+        radius: 10,
+        fill: new ol.style.Fill({ color: color }), 
+        stroke: new ol.style.Stroke({ color: 'black', width: 1 }),
+      }),
+    })
+  ];
+};
+
+
 //let newestFeature = null; 
 //let newestFeatures = await loadNewestFeature();
 
 
-
+/**
+ * Loads and returns data from .geojson file. 
+ * @returns {object}
+ */
 async function loadGEOJSON() {
   const response = await fetch(dataFile);
   const data = await response.json();
@@ -104,17 +164,24 @@ async function loadGEOJSON() {
   return data; 
 }
 
-// find newest feauture 
+
+// new array for newestFeatures
+var newestFeatures = []; 
+
+
+/**
+ * Find newest feature for each point ID. 
+ * @returns {object}
+ */
 async function loadNewestFeature() {
   try {
     const data = await loadGEOJSON(); 
     const newestFeaturesByID = {};
 
-    // sort features by ID and time 
+    // get newest features by ID 
     data.features.forEach(feature => {
       const id = feature.properties.Name; 
       const featureTime = new Date(feature.properties.time); 
-      //console.log(id);
       
       if (!newestFeaturesByID[id] || featureTime > new Date(newestFeaturesByID[id].properties.time)) {
         newestFeaturesByID[id] = feature;
@@ -122,127 +189,99 @@ async function loadNewestFeature() {
     });
 
     newestFeatures = Object.values(newestFeaturesByID);
-    console.log(newestFeatures); 
-    smarttrack_air.changed();
-    return newestFeatures; 
-    /*const sortedFeatures = data.features.sort((a, b) => 
-      new Date(b.properties.time) - new Date(a.properties.time)
-    );
-    newestFeature = sortedFeatures[0];*/
-
-    // refresh layer
-    
+    smarttrackAir.changed();
+    console.log("newestFeatures: ", newestFeatures); 
+    return newestFeatures;  
 
   } catch (error) {
     console.error("Fehler beim Laden der GeoJSON-Daten:", error);
   }
 }
 
-const smarttrack_air = new ol.layer.Vector({
-  source: vectorSource_air,
+
+/**
+ * Creates "Air Quality" layer with newest feature points. 
+ */
+const smarttrackAir = new ol.layer.Vector({
+  source: vectorSourceAir,
   title: "Air Quality",
   style: function (feature) {
-    // Prüfen, ob das aktuelle Feature in newestFeatures enthalten ist
-    const isNewest = newestFeatures.some(newestFeature =>
+    // check if feature is in newestFeatures
+    const isNewest = (Array.isArray(newestFeatures) ? newestFeatures : []).some(newestFeature =>
       feature.getId() === newestFeature.id || 
       feature.get('id') === newestFeature.id
     );
 
-    // Falls das Feature in newestFeatures ist, wende den Style an, sonst kein Style
+    // only apply style to newestFeatures 
     return isNewest ? pointStyle(feature) : null;
   },
   visible: true,
 });
 
-let newestFeatures = loadNewestFeature(); // -------------------------------------------------------------------------------------------> was von beiden ist hier jetzt besser? 
+
+newestFeatures = loadNewestFeature(); 
 //let feature = loadNewestFeature();
 
-// Function to toggle legend visibility
+
+/**
+ * Toggles legend and searchbar visibility based on layer visibility. 
+ */
 function toggleVisibility() {
-  var air_legend = document.getElementById('air_legend');
-  var air_searchbar = document.getElementById('searchbar'); 
-  if (smarttrack_air.getVisible()) {
-    air_legend.style.display = 'block';
-    air_searchbar.style.display = 'block'; 
+  var airLegend = document.getElementById('air_legend');
+  var airSearchbar = document.getElementById('searchbar'); 
+  if (smarttrackAir.getVisible()) {
+    airLegend.style.display = 'block';
+    airSearchbar.style.display = 'block'; 
   } else {
-    air_legend.style.display = 'none';
-    air_searchbar.style.display = 'none'; 
+    airLegend.style.display = 'none';
+    airSearchbar.style.display = 'none'; 
   }
 }
 
-// Add event listener to toggle legend visibility when layer visibility changes
-smarttrack_air.on('change:visible', toggleVisibility);
 
-// Initial call to set the correct visibility on load
+// Add event listener to toggle legend and searchbar visibility when layer visibility changes. 
+smarttrackAir.on('change:visible', toggleVisibility);
+
+// Initial call to set the correct visibility on load.
 toggleVisibility();
 
-console.log(vectorSource_air.getFeatures());
+// show smarttrackAir layer 
+overlayGroup.getLayers().push(smarttrackAir);
 
-overlayGroup.getLayers().push(smarttrack_air);
 
-// change size of points on map 
-const pointStyleLarge = function(feature) { 
-  const airQuality = feature.get('Luftqualitätsindex');
-  const color = getColorByAirQuality(airQuality);
-  //console.log("Color transparency: ", (color + '80')); 
+var hoveredFeature = null;
+var selectedFeature = null;
 
-  return [
-    new ol.style.Style({
-      geometry: function (feature) {
-        return feature.getGeometry();
-      },
-      image: new ol.style.Circle({
-        radius: 30,
-        fill: new ol.style.Fill({ color: color + '80' }), // Halbtransparenter Kreis
-        stroke: new ol.style.Stroke({ color: color, width: 1 }),
-      }),
-    }),
-    new ol.style.Style({
-      geometry: function (feature) {
-        return feature.getGeometry();
-      },
-      image: new ol.style.Circle({
-        radius: 10,
-        fill: new ol.style.Fill({ color: color }), // Kleinere volle Farbe
-        stroke: new ol.style.Stroke({ color: 'black', width: 1 }),
-      }),
-    })
-  ];
-};
-
-let hoveredFeature = null;
-let selectedFeature = null;
-
-// Add event listener for pointermove to handle hover effect
+// Add an event listener on pointermove to handle hover effect of feature points. 
 map.on('pointermove', function(evt) {
-  if (evt.dragging) {
-    return;
-  }
+  if (evt.dragging) return;
+
   var pixel = map.getEventPixel(evt.originalEvent);
   var hit = map.hasFeatureAtPixel(pixel, function(feature, layer) {
-    return layer === smarttrack_air;
+    return layer === smarttrackAir;
   });
 
   map.getTargetElement().style.cursor = hit ? 'pointer' : '';
 
-  // bei der Mausbewegung wird der Kreis wieder kleiner aber der andere nicht groß --> check das mal Pauline 
-  map.forEachFeatureAtPixel(pixel, function(feature, layer) {
-    if (layer === smarttrack_air) {
-      if (hoveredFeature && hoveredFeature !== feature && hoveredFeature !== selectedFeature) {
-        hoveredFeature.setStyle(pointStyle(hoveredFeature));
-      }
+
+  let newHoveredFeature = null;
+
+  // set pointStyleLarge for each feature in smarttrackAir that is hovered and is not selected
+  map.forEachFeatureAtPixel(pixel, (feature, layer) => {
+    if (layer === smarttrackAir) {
+      newHoveredFeature = feature;
       if (feature !== selectedFeature) {
         feature.setStyle(pointStyleLarge(feature));
       }
-      //feature.setStyle(pointStyleLarge(feature));
-      hoveredFeature = feature;
     }
   });
 
-  if (!hit && hoveredFeature && hoveredFeature !== selectedFeature) {
+  // set pointStyle for previously hovered feature if its no longer hovered or selected
+  if (hoveredFeature && hoveredFeature !== selectedFeature && hoveredFeature !== newHoveredFeature) {
     hoveredFeature.setStyle(pointStyle(hoveredFeature));
-    hoveredFeature = null;
   }
+
+  hoveredFeature = newHoveredFeature;
 });
 
 
@@ -250,28 +289,28 @@ map.on('pointermove', function(evt) {
 initSearch(); 
 
 
-/* by Pauline Thiele end */
-
 ///////////////////////////////////////////
 // Popup handler
 function initPopUp()
 {
+  // -- from Alberding GmbH
   var container = document.getElementById('popup');
   var containerAQ = document.getElementById('popup-AQ');
   var content = document.getElementById('popup-content');
 
-  /* by Pauline Thiele begin */
+  // -- from Pauline Thiele 
   var contentAQ = document.getElementById('popup-content-AQ'),
-    closer = document.getElementById('popup-closer'),
-    closerAQ = document.getElementById('popup-closer-AQ'); 
+      closer = document.getElementById('popup-closer'),
+      closerAQ = document.getElementById('popup-closer-AQ'); 
 
-  const popUpSection = document.getElementById('popUpSection'),
-    dailogHM3301 = document.getElementById('dailogHM3301'),
-    dailoglastRow = document.getElementById('dialogLastRow');
-  /* by Pauline Thiele end */
+  // -- from Pauline Thiele 
+  const popupTable = document.getElementById('popupTable'),
+        colHM3301 = document.getElementById('colHM3301'),
+        tableLastRow = document.getElementById('tableLastRow');
 
   /**
     * Create an overlay to anchor the popup to the map.
+    * -- from Alberding GmbH
     */
   var overlay = new ol.Overlay(/** @type {olx.OverlayOptions} */ ({
     element: container,
@@ -286,7 +325,7 @@ function initPopUp()
   
   /**
     * Create an overlay for AQ to anchor the popup to the map.
-    * by Pauline Thiele
+    * -- from Pauline Thiele
     */
   var overlayAQ = new ol.Overlay(/** @type {olx.OverlayOptions} */ ({
     element: containerAQ,
@@ -302,6 +341,7 @@ function initPopUp()
   /**
     * Add a click handler to hide the popup.
     * @return {boolean} Don't follow the href.
+    * -- from Alberding GmbH
     */
   closer.onclick = function() {
     overlay.setPosition(undefined);
@@ -312,7 +352,7 @@ function initPopUp()
   /**
     * Add a click handler to hide the popup.
     * @return {boolean} Don't follow the href.
-    * by Pauline Thiele 
+    * -- from Pauline Thiele 
     */
   closerAQ.onclick = function() {
     overlayAQ.setPosition(undefined);
@@ -324,7 +364,7 @@ function initPopUp()
   
   /**
     * Add a click handler to the map to render the popup.
-    * hier nochmal durchgehen und checken was von mir ist und was nicht!!!!!!!!!!!!!!!!!!!!!!!!!
+    * -- from Alberding GmbH, Pauline Thiele --> sections are marked with comments 
     */
   map.on('singleclick', function(evt) {
     var coordinate = evt.coordinate;
@@ -332,7 +372,9 @@ function initPopUp()
         function(feature, layer) {
           return { feature, layer };
         });
-    
+      
+      // -- from Pauline Thiele begin 
+
       if (result) {
         var feature = result.feature;
         var layer = result.layer;
@@ -343,65 +385,71 @@ function initPopUp()
         console.log('No feature found at the clicked position.');
       }
  
-    /* macht gerade nichts!!! 
+    /* 
     var hdms = ol.coordinate.toStringHDMS(ol.proj.transform(
         coordinate, 'EPSG:3857', 'EPSG:4326'));*/
 
-    /* warum habe ich das rausgenommen???? 
+    /* 
     if (feature && feature.get('description') !== undefined) {
       content.innerHTML = feature.get('description');
       overlay.setPosition(coordinate);
       return;
     }
     else if */
+    
     const sidebarwidth = document.getElementById("sidebar").offsetWidth;
-    if (feature && feature.getProperties() !== undefined) {
-      if (layer === smarttrack_air) {
+
+    if (feature && feature.getProperties() !== undefined) { // -- from Alberding GmbH 
+      if (layer === smarttrackAir) {
         
         console.log("sidebarwidth: ", sidebarwidth); 
-        if ((selectedFeature && selectedFeature !== feature && sidebarwidth > 10) || (selectedFeature && selectedFeature === feature && sidebarwidth > 10)) { // wenn sidebar offen auf anderen Punkt klicken 
+
+        // if feature was selected and sidebar is open 
+        if ((selectedFeature && selectedFeature !== feature && sidebarwidth > 10) || (selectedFeature && selectedFeature === feature && sidebarwidth > 10)) { 
+          
+          // reset point style and open sidebar for selected feature 
           selectedFeature.setStyle(pointStyle(selectedFeature));
           openSidebar(feature);
           selectedFeature = feature;
         }
-        else if ((selectedFeature && selectedFeature === feature) || (selectedFeature !== feature && sidebarwidth <= 10)) { // klick auf einen Punkt wenn nichts oder popup von anderem Punkt offen ist 
-          /*content.innerHTML = ''; 
-          contentAQ.innerHTML = ''; */
+
+        // if feature was selected and sidebar is not open --> show popup 
+        else if ((selectedFeature && selectedFeature === feature) || (selectedFeature !== feature && sidebarwidth <= 10)) { 
+          
           // Popup logic for air.geojson
           let insertedContent = document.querySelectorAll(".insertedContent");
 
+          // remove all existing content 
           if(insertedContent) {
             for (var i = 0; i < insertedContent.length; i++)  insertedContent[i].remove();
           }
 
           console.log(feature.getProperties());
           
-          let html = `  <p class="dialogTimestamp insertedContent">Last Update at: <br> ${feature.get('time') || 'Unknown'}</p>
-                        <p id="station" class ='dialogContent insertedContent'>Station: ${feature.get('Name') || 'Unknown'}</p>
-                        <p id="AQI" class ='dialogContent insertedContent'>Air Quality Index: </p>
-                        <p id="dailogMv" class="dialogContent insertedContent">Measurement Values:</p>`;
-          popUpSection.insertAdjacentHTML('afterbegin', html);
+          // create new content 
+          let html = `  <p class="popupTimestamp insertedContent">Last Update at: <br> ${feature.get('time') || 'Unknown'}</p>
+                        <p id="station" class ="popupContent insertedContent">Station: ${feature.get('Name') || 'Unknown'}</p>
+                        <p id="AQI" class ="popupContent insertedContent">Air Quality Index: </p>
+                        <p id="popupMV" class="popupContent insertedContent">Measurement Values:</p>`;
+          popupTable.insertAdjacentHTML('afterbegin', html);
 
-          html = `  <td rowspan="2" class ='insertedContent'>${feature.get('NO2') || 'Unknown'}</td>
-                    <td rowspan="2" class ='insertedContent'>${feature.get('CO') || 'Unknown'}</td>`;
-          dailogHM3301.insertAdjacentHTML('afterend', html);
+          html = `  <td rowspan="2" class ="insertedContent">${feature.get('NO2') || 'Unknown'}</td>
+                    <td rowspan="2" class ="insertedContent">${feature.get('CO') || 'Unknown'}</td>`;
+          colHM3301.insertAdjacentHTML('afterend', html);
 
-          //TODO: Klassennamen nicht mit Unterstrichen!! 
-          html = `  <td class ='insertedContent'>${feature.get('SD:PM2_5') || 'Unknown'}</td>
-                    <td class="tb_child_even insertedContent">${feature.get('GM:PM2_5_Atm') || 'Unknown'}</td> 
-                    <td class="tb_child_uneven insertedContent">${feature.get('SD:PM10') || 'Unknown'}</td>
-                    <td class ='insertedContent'>${feature.get('GM:PM10_Atm') || 'Unknown'}</td>`;
-          dailoglastRow.insertAdjacentHTML('afterbegin', html);
+          html = `  <td class ="insertedContent">${feature.get('SD:PM2_5') || 'Unknown'}</td>
+                    <td class="tbChildEven insertedContent">${feature.get('GM:PM2_5_Atm') || 'Unknown'}</td> 
+                    <td class="tbChildUneven insertedContent">${feature.get('SD:PM10') || 'Unknown'}</td>
+                    <td class ="insertedContent">${feature.get('GM:PM10_Atm') || 'Unknown'}</td>`;
+          tableLastRow.insertAdjacentHTML('afterbegin', html);
 
           var AqIndex = document.getElementById("AQI");
           AqIndex.insertBefore(createColoredAqDiv(feature.getProperties()['Luftqualitätsindex']), AqIndex.children[0]); 
 
           window.openSidebarHelper = function() {
             openSidebar(feature);
-            //closer.onclick();
             overlayAQ.setPosition(undefined);
             closerAQ.blur();
-            //feature.setStyle(pointStyleLarge(feature));
           };
 
           console.log("feature: ", feature); 
@@ -412,17 +460,17 @@ function initPopUp()
           
           selectedFeature = feature;
           selectedFeature.setStyle(pointStyleLarge(selectedFeature));
-          
 
-          //contentAQ.innerHTML = airQualityContent;
           overlayAQ.setPosition(coordinate);
           return;
         }
       } else {
         content.innerHTML = ''; 
         contentAQ.innerHTML = ''; 
-
-        // Popup logic for points.geojson
+        
+        // -- from Pauline Thiele end 
+        
+        // Popup logic for points.geojson -- from Alberding GmbH 
         var table = "<table class=\"mapMarkerInfo\">\n";
         var yourobject = feature.getProperties();
         let valid = 0;
